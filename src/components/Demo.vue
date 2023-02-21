94fa5871a23ae9e683fd3a04412a382421ffd76f
Joe Chen <joe824111@gmail.com>
2023-02-21 09:53:09 +0800

    02_setup的props與context

---
+++ src/components/Demo.vue
@@ -0,0 +1,22 @@
+<template>
+  <h3>{{ msg }} {{ name }}</h3>
+  <button @click="test">執行Demo組件的hello事件</button>
+</template>
+
+<script>
+  import { ref, reactive } from "vue";
+  export default {
+    name: "App",
+    props: ["msg", "name"],
+    emits: ["hello"],
+    setup(props, context) {
+      console.log(props);
+      console.log(context);
+
+      const test = () => {
+        context.emit("hello", 666);
+      };
+      return { test };
+    },
+  };
+</script>
