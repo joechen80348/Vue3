217fedbc78d4a682eeefbd796052a0c1d480c456
Joe Chen <joe824111@gmail.com>
2023-02-21 15:28:15 +0800

    06_拆分 todo.html 組件

---
+++ 06_拆分todo.html組件/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div class="container">
+    <MyHeader />
+    <MyList />
+  </div>
+</template>
+
+<script>
+  import MyHeader from "./components/MyHeader.vue";
+  import MyList from "./components/MyList.vue";
+
+  export default {
+    name: "App",
+    components: { MyHeader, MyList },
+  };
+</script>
+
+<style scoped>
+  .container {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  }
+</style>
