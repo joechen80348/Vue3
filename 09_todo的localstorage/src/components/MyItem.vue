9e2c1d56891e75e7647dfe36ce1f5562d490c0da
Joe Chen <joe824111@gmail.com>
2023-02-23 09:07:36 +0800

    09_todo的localstorage

---
+++ 09_todo的localstorage/src/components/MyItem.vue
@@ -0,0 +1,136 @@
+<template>
+  <li class="todo-item">
+
+    <input
+      type="checkbox"
+      :checked="todo.done"
+      @click="todo.done=== !todo.done"
+    >
+
+    <p class="text">{{ todo.title }}</p>
+    <div class="buttons">
+      <button class="edit">
+        <i class="fas fa-edit"></i>
+      </button>
+      <button
+        class="delete"
+        @click="handleDelete()"
+      >
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: "MyItem",
+    props: ["todo", "deleteTodo", "index"],
+    setup(props) {
+      function handleDelete() {
+        if (confirm("確定刪除嗎?")) {
+          props.deleteTodo(props.index);
+        }
+      }
+
+      return { handleDelete };
+    },
+  };
+</script>
+
+<style scoped>
+  .todo-item {
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    margin-bottom: 10px;
+    background-color: #f1f1f1;
+    border-radius: 5px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  }
+  .todo-item .text {
+    font-size: 20px;
+    color: #333;
+    margin: 0;
+    flex: 1;
+  }
+  .todo-item .buttons {
+    display: flex;
+    align-items: center;
+  }
+  .todo-item .buttons button {
+    padding: 10px;
+    margin-left: 5px;
+    background-color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  .todo-item .buttons .edit {
+    color: #2ecc71;
+  }
+  .todo-item .buttons .delete {
+    color: #e74c3c;
+  }
+  .todo-item.completed .text {
+    text-decoration: line-through;
+    color: #999;
+  }
+  .todo-item .check-box {
+    display: flex;
+    align-items: center;
+    margin-right: 10px;
+  }
+  .todo-item .check-box input[type="checkbox"] {
+    width: 25px;
+    height: 25px;
+    margin: 0;
+    cursor: pointer;
+  }
+  .todo-item .check-box .checkmark {
+    width: 25px;
+    height: 25px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin-right: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  }
+  .todo-item .check-box .checkmark i {
+    font-size: 20px;
+    color: #fff;
+  }
+
+  /* Add styles for the completed checkbox */
+  .todo-item .check-box .checkmark.checked {
+    background-color: #2ecc71;
+    border-color: #2ecc71;
+  }
+  .todo-item .check-box .checkmark.checked i {
+    display: block;
+  }
+  /* Add styles for the edit form */
+  .edit-form {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+  .edit-form input[type="text"] {
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    flex: 1;
+    margin-right: 10px;
+  }
+  .edit-form button {
+    padding: 10px 20px;
+    background-color: #2ecc71;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+</style>
+
---
+++ 09_todo的localstorage/src/components/MyList.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul id="todo-list">
+    <MyItem
+      v-for="(todoObj, index) in todos"
+      :key="todoObj.id"
+      :todo="todoObj"
+      :deleteTodo="deleteTodo"
+      :index="index"
+    />
+  </ul>
+</template>
+
+<script>
+  import MyItem from "./MyItem.vue";
+
+  export default {
+    name: "MyList",
+    components: { MyItem },
+    props: ["todos", "deleteTodo"],
+    setup() {
+      // function checkTodo(id) {
+      //   this.todos.foreach((todo) => {
+      //     if (todo.id === id) todo.done = !todo.done;
+      //   });
+      // }
+
+      return {};
+    },
+  };
+</script>
+
